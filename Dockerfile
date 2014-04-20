4a03b8a85708d968528e49061d84131b3a959039
Lele Long <schemacs@gmail.com>
2014-04-20 10:42:33 +0800

    Fix through to deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-from ubuntu:12.04
+#from ubuntu:12.04
+from saucy
 maintainer Shipyard Project "http://shipyard-project.com"
 run apt-get update
 run apt-get install -y curl
-run curl https://get.docker.io/builds/Linux/x86_64/docker-latest -o /usr/local/bin/docker
+#run curl https://get.docker.io/builds/Linux/x86_64/docker-latest -o /usr/local/bin/docker
+add docker-latest /usr/local/bin/docker
 run chmod +x /usr/local/bin/docker
 add run.sh /usr/local/bin/run
 entrypoint ["/usr/local/bin/run"]
--- run.sh
+++ run.sh
@@ -39,27 +39,35 @@ function purge {
 }
 
 if [ "$ACTION" = "setup" ] ; then
+
+    cat /archives/ubuntu.tar | docker -H unix://docker.sock import - ubuntu
+    cat /archives/shipyard_db.tar | docker -H unix://docker.sock import - shipyard/db
+    cat /archives/shipyard_latest.tar | docker -H unix://docker.sock import - shipyard/shipyard:latest
+    cat /archives/shipyard_lb.tar | docker -H unix://docker.sock import - shipyard/lb
+    cat /archives/shipyard_redis.tar | docker -H unix://docker.sock import - shipyard/redis
+    cat /archives/shipyard_router.tar | docker -H unix://docker.sock import - shipyard/router
+
     echo "
 Using $TAG tag for Shipyard
 This may take a moment while the Shipyard images are pulled..."
     echo "Starting Redis..."
-    redis=$(docker -H unix://docker.sock run -i -t -d -p 6379:6379 -name shipyard_redis shipyard/redis)
-    sleep 2
+    redis=$(docker -H unix://docker.sock run -i -t -d -p 6379:6379 --name shipyard_redis shipyard/redis /usr/local/bin/redis-server /etc/redis.conf)
+    sleep 5
     echo "Starting App Router..."
-    router=$(docker -H unix://docker.sock run -i -t -d -p 80 -link shipyard_redis:redis -name shipyard_router shipyard/router)
-    sleep 2
+    router=$(docker -H unix://docker.sock run -i -t -d -p 80 --link shipyard_redis:redis --name shipyard_router shipyard/router /bin/sh -e /usr/local/bin/run)
+    sleep 5
     echo "Starting Load Balancer..."
-    lb=$(docker -H unix://docker.sock run -i -t -d -p 80:80 -link shipyard_redis:redis -link shipyard_router:app_router -name shipyard_lb shipyard/lb)
-    sleep 2
+    lb=$(docker -H unix://docker.sock run -i -t -d -p 80:80 --link shipyard_redis:redis --link shipyard_router:app_router --name shipyard_lb shipyard/lb /bin/sh -e /usr/local/bin/run)
+    sleep 5
     echo "Starting DB..."
     EXTRA_DB_ARGS=""
     if [ ! -z "$DB_HOST_VOLUME" ] ; then
         EXTRA_DB_ARGS="-v $DB_HOST_VOLUME:/var/lib/postgresql"
     fi
-    db=$(docker -H unix://docker.sock run -i -t -d -p 5432 -e DB_NAME=shipyard -e DB_USER=shipyard -e DB_PASS=$DB_PASS $EXTRA_DB_ARGS -name shipyard_db shipyard/db)
+    db=$(docker -H unix://docker.sock run -i -t -d -p 5432 -e DB_NAME=shipyard -e DB_USER=shipyard -e DB_PASS=$DB_PASS $EXTRA_DB_ARGS --name shipyard_db shipyard/db /bin/bash -e /usr/local/bin/run)
     sleep 5
     echo "Starting Shipyard"
-    shipyard=$(docker -H unix://docker.sock run -i -t -d -p 8000:8000 -link shipyard_db:db -link shipyard_redis:redis -name shipyard -e ADMIN_PASS=$ADMIN_PASS -e DEBUG=$DEBUG shipyard/shipyard:$TAG app master-worker)
+    shipyard=$(docker -H unix://docker.sock run -i -t -d -p 8000:8000 --link shipyard_db:db --link shipyard_redis:redis --name shipyard -e ADMIN_PASS=$ADMIN_PASS -e DEBUG=$DEBUG shipyard/shipyard:$TAG /app/.docker/run.sh app master-worker)
     echo "
 Shipyard Stack Deployed
 
